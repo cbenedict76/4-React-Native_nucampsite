3ce28a92c4ea7468ef5305d2fdcfd188a3e92152
Corey Benedict <corey.benedict@gmail.com>
2021-06-22 19:08:25 -0400

    Navigation Part 2

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
+import { PARTNERS } from "../shared/partners";
 
+function RenderItem({ item }) {
+  if (item) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        image={require("./images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+}
 class Home extends Component {
   static navigationOptions = {
     title: "Home",
